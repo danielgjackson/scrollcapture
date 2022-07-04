a0334a2ba862ffe23613ff3e134c76d126b112c1
Daniel Jackson <email@danieljackson.co.uk>
2022-07-04 09:38:40 +0100

    Default crop values

---
+++ crop-params.generated.sh
@@ -0,0 +1,7 @@
+# Warning: This file will be overwritten by the crop calibration command
+
+# Empty crop
+#crop_top=0; crop_bottom=0; crop_left=0; crop_right=0;
+
+# Approximate sizes for MBP-15 Safari
+crop_top=182; crop_bottom=158; crop_left=115; crop_right=115;
